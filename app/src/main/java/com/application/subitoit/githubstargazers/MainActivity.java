e4312a64ce34edfe5af500482cd20c79bc7dc397
davide-ennova <davide.lamanna@ennova.it>
2017-03-04 16:57:57 +0100

    added all necessary libs - added two activity to handle data insertion and stargazer list view

--- app/src/main/java/com/application/subitoit/githubstargazers/MainActivity.java
+++ app/src/main/java/com/application/subitoit/githubstargazers/MainActivity.java
@@ -1,13 +1,50 @@
 package com.application.subitoit.githubstargazers;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Unbinder unbinder;
+
+    @BindView(R.id.findButtonId)
+    Button findButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+        onInitView();
+    }
+
+    @Override
+    protected void onResume() {
+        unbinder = ButterKnife.bind(this);
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (unbinder != null)
+            unbinder.unbind();
+        super.onDestroy();
+    }
+
+    private void onInitView() {
+        findButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, StargazersListActivity.class));
     }
 }
--- app/src/main/java/com/application/subitoit/githubstargazers/StargazersListActivity.java
+++ app/src/main/java/com/application/subitoit/githubstargazers/StargazersListActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class StargazersListActivity extends AppCompatActivity {
-
+    //dadoz/SelectCardViewPrototype
+    // API URL https://api.github.com/repos/{owner}/{repo}/stargazers
+    //Object -> avatar and username
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_stargazers_list);
     }
 }
