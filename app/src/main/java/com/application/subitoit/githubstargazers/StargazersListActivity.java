ca5ed861b902d2e66c2b043948e6218b84e5cde0
davide-ennova <davide.lamanna@ennova.it>
2017-03-04 15:53:42 +0100

    initial commit

---
+++ app/src/main/java/com/application/subitoit/githubstargazers/StargazersListActivity.java
@@ -0,0 +1,13 @@
+package com.application.subitoit.githubstargazers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StargazersListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
